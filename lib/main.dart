eca0ce9ec575e1111314d4a5b9337d4a157b0dea
gusjabungaf <gusjabungaf@gmail.com>
2024-11-26 09:31:55 +0000

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,309 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const DividerComponent(title: 'Flutter Widget Fundamental'),
+    );
+  }
+}
+
+// HORIZONTAL COMPONENT
+class HorizontalDividerComponent extends StatelessWidget {
+  const HorizontalDividerComponent({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Horizontal Divier'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+                child: const Center(
+                  child: Text(
+                    'A',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Divider(
+              color: Color.fromARGB(255, 0, 0, 0),
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+                child: const Center(
+                  child: Text(
+                    'B',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// VERTICAL COMPONENT
+class VerticalDividerComponent extends StatelessWidget {
+  const VerticalDividerComponent({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vertical Divider'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+                child: const Center(
+                  child: Text(
+                    'A',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const VerticalDivider(
+              color: Color.fromARGB(255, 0, 0, 0),
+              width: 20,
+              thickness: 2, // Ketebalan divider
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+                child: const Center(
+                  child: Text(
+                    'B',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// MOVIE CARD COMPONENT WITH HORIZONTAL AND VERTICAL DIVIDER
+class DividerComponent extends StatelessWidget {
+  const DividerComponent({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Divider'),
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(height: 10),
+
+              // Vertical Divider
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6)),
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    IntrinsicHeight(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(width: 10),
+                          Icon(Icons.search),
+                          SizedBox(width: 10),
+                          Expanded(
+                              child: TextField(
+                            style: TextStyle(fontSize: 16),
+                            decoration: InputDecoration(
+                              hintText: "Cari anime ",
+                              focusedBorder: InputBorder.none,
+                            ),
+                          )),
+                          SizedBox(width: 5),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Container(height: 10),
+
+              // Horizontal Divider : Custom
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6)),
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Image.network(
+                        'https://utfs.io/f/RPcRbvHcnI5H191AgrlsvMCXFyVxh7PKOb28963ktgz5NAEJ'),
+                    Container(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("The Wind Rises",
+                              style:
+                                  Theme.of(context).textTheme.headlineMedium),
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: Text("2013",
+                                style: Theme.of(context).textTheme.titleMedium),
+                          ),
+                          const Text(
+                              'Jiro dreams of flying and designing beautiful airplanes, inspired by the famous Italian aeronautical designer Caproni. Nearsighted from a young age and thus unable to become a pilot, Jiro joins the aircraft division of a major Japanese engineering company in 1927. His genius is soon recognized, and he grows to become one of the world most accomplished airplane designers.'),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const Text(
+                            "Genre",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Container(height: 5),
+                          Row(
+                            children: [
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.grey[300],
+                                  elevation: 0,
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 0, horizontal: 20),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(18.0)),
+                                ),
+                                child: const Text("History"),
+                                onPressed: () {},
+                              ),
+                              Container(width: 8),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.grey[300],
+                                  elevation: 0,
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 0, horizontal: 20),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(18.0)),
+                                ),
+                                child: const Text("Romance"),
+                                onPressed: () {},
+                              ),
+                              Container(width: 8),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.grey[300],
+                                  elevation: 0,
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 0, horizontal: 20),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(18.0)),
+                                ),
+                                child: const Text("Melodrama"),
+                                onPressed: () {},
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
